5c2627447aaa956c93ba1213b178bb281e6df206
ozgur <ozgurcokckn@gmail.com>
2023-03-29 19:46:38 +0300

    skeleton ready

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,16 @@ import Head from "next/head";
 
 import { api } from "~/utils/api";
 
-import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 const Home: NextPage = () => {
-  const { isSignedIn, user } = useUser();
+  const { isSignedIn } = useUser();
 
-  const { data } = api.posts.getAll.useQuery();
+  const { data, isLoading } = api.posts.getAll.useQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (!data) return <div>Something went wrong.</div>;
 
   return (
     <>
@@ -20,8 +24,19 @@ const Home: NextPage = () => {
       <main className="flex h-screen justify-center">
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
           <div className="flex border-b border-slate-400 p-4">
-            {!isSignedIn && <SignInButton />}
+            {!isSignedIn && (
+              <div className="flex justify-center">
+                <SignInButton />
+              </div>
+            )}
             {isSignedIn && <SignOutButton />}
+          </div>
+          <div>
+            {data?.map((post) => (
+              <div className="border-b border-slate-400 p-8" key={post.id}>
+                {post.content}
+              </div>
+            ))}
           </div>
         </div>
       </main>
